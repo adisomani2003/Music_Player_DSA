4a375fb5bf93318b53da09268737456673fb7c3c
adisomani2003 <f20202049@hyderabad.bits-pilani.ac.in>
2022-04-21 10:38:29 +0530

    Restructured and Addition Functions

--- main.cpp
+++ main.cpp
@@ -1,62 +1,122 @@
 #include<iostream>
-#include<string.h>
-#include<stdlib.h>
-#include<stdio.h>
-#include<fstream>
-
 using namespace std;
 
-struct node
-{
-    char song_name[100];
-    struct node *next;
-    struct node *prev;
-}*top, *temp, *top1;
 
-void convertToFile(char a[]){
-    fstream f1; //fstream helps us both read and write to files
-    f1.open("playlist.txt",ios::out|ios::app); //out helps us open file for output operations and app for adding content to current file
-    f1<<a<<endl;
-    f1.close();//Closing the file
+class DoublyLinkedNode{
+    public:
+    string data;
+    DoublyLinkedNode *next;
+    DoublyLinkedNode *prev;
+
+    //constructor
+    DoublyLinkedNode(string data, DoublyLinkedNode *next, DoublyLinkedNode *prev);
 };
-void addNode(struct node*temp1)
-{
-    char user_song[100];
-    while (temp1->next !=NULL)//Traverse the whole LL to add a node in the end
-    {
-        temp1 = temp1->next;
-
+DoublyLinkedNode::DoublyLinkedNode(string data, DoublyLinkedNode *next, DoublyLinkedNode *prev){
+        this->data = data;
+        this->next = next;
+        this->prev = prev;
     }
-    temp1->next = (struct node*)malloc(sizeof(struct node)); //a node pointer gets memory allocated equivalent to size of node
+
+class DoublyLinkedList{
+    protected:
+
+    DoublyLinkedNode *current;
+    DoublyLinkedNode *head;
+    DoublyLinkedNode *tail;
+    int totalSongs;
+
+    public:
+    DoublyLinkedList();
+    void addSong(const string &newSongName);
+    void printAll();
+    void getCurrentSong();
+    void addSongBeginning(const string &newSongName);
+    void addSongLocation(const string &newSongName, int position);
+    void deleteSongBeginning();
+    void deleteSongLocation();
+    void deleteSongEnd();
+};
+
+DoublyLinkedList::DoublyLinkedList(){
     
-    temp1->prev = temp1;
-    temp1 = temp1->next;
-    cout<<"\nEnter Song-  ";
-    cin>>user_song;
-    strcpy(temp1->song_name, user_song);
-    convertToFile(user_song);
-    temp1->next = NULL;
+        head = NULL;
+        tail = NULL;
+        totalSongs = 0;
 
 };
-void addFile(struct node*temp1, string user_song){
-    while(temp1->next!=NULL){
-        temp1 = temp1->next;
+
+void DoublyLinkedList::getCurrentSong(){
+    cout<<current->data<<endl;
+}
+
+void DoublyLinkedList::addSong(const string &newSongName){
+    if(totalSongs==0){
+        head = new DoublyLinkedNode(newSongName, NULL, NULL);
+        head->next = tail;
+        current = head;
+
+    }
+    else{
+        DoublyLinkedNode *newNode = new DoublyLinkedNode(newSongName, current->next, current);
+        current->next->prev = newNode;
+        current->next = newNode;
+        current = current->next;
+        
+    }
+    totalSongs++;
+}
+void DoublyLinkedList::addSongBeginning(const string &newSongName){
+    DoublyLinkedNode *newNode = new DoublyLinkedNode(newSongName, head, NULL);
+    head->prev = newNode;
+    head = newNode;
+    totalSongs++;
+
+}
+
+void DoublyLinkedList::addSongLocation(const string &newSongName, int position){
+    DoublyLinkedNode *newNode = new DoublyLinkedNode(newSongName, NULL, NULL); //seting up NULL for now
+    if(position <0){
+        cout<<"Position should be >=0"<<endl;
+
+    }
+    else if (position == 0)//basically new node is head
+    {
+        addSongBeginning(newSongName);
+
+    }
+    else{
+        DoublyLinkedNode*temp = head;
+        for(int i = 0; i < position; i++){//Maybe need to add a case to add node at end using this function
+            if(temp != NULL)
+                temp = temp->next;
+            if(temp == NULL){
+                cout<<"Position entered is greater then current length of linked list"<<endl;
+            }
+
+        }
+        if(temp != NULL){
+            temp->next = newNode->next;
+            newNode->prev = temp;
+            temp->next = newNode;
+        }
     }
 
-    temp1->next = (struct node*)malloc(sizeof(struct node));
-    temp1->prev = temp1;
-    temp1 = temp1->next;
-    strcpy(temp1->song_name, user_song.c_str()); //c_str converts the usersong to c type string
-    temp1->next = NULL;
+}
+
+void DoublyLinkedList::printAll(){
+
+    DoublyLinkedNode *temp = head;
+    while (temp != NULL)
+    {
+        cout<<temp->data<<endl;
+        temp = temp->next;
+    }
+    
 }
 
 int main(){
-    char song[100];
-    struct node *start,*hold;
-    start=(struct node *) malloc(sizeof(struct node));
-    cout<<"**WELCOME**"<<endl;
-
-    cout<<"Enter your playlist name-  "<<endl;
-    cout<<"Replace speces with _"<<endl;
-
+    DoublyLinkedList *testPlaylist = new DoublyLinkedList();
+    testPlaylist->addSong("Song1");
+    testPlaylist->addSong("Song2");
+    testPlaylist->printAll();
 }
